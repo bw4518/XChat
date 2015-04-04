ac9dbb16d86614542612b096741144b8d6f4eff6
culb <jessejlutz@gmail.com>
2015-04-04 15:47:35 -0400

    Additions
    Handle private messages and parts. Few other tweaks to improve the
    events.

--- 4x4EvolutionNormalizer.pl
+++ 4x4EvolutionNormalizer.pl
@@ -13,7 +13,7 @@ my @networkNames = qw( fuzzy personal );
 #Don't touch
 my %nnr = ();
 
-#|BEGIN| Channel Messages
+# |BEGIN| Channel Messages
 for my $event ( 'Channel Message', 'Channel Msg Hilight' ) {
 	hook_print( $event, \&eventChannelMessage, { 'data' => $event } );
 }
@@ -45,10 +45,40 @@ sub eventChannelMessage {
 	}
 	return EAT_NONE;
 }
+# |END| Channel Message
 
-#|END| Channel Message
 
-#|BEGIN| Channel Modes(h,o,v)
+# |BEGIN| Private Messages
+for my $event ( 'Private Message', 'Private Message to Dialog' ) {
+	hook_print( $event, \&eventPrivateMessage, { 'data' => $event } );
+}
+
+sub eventPrivateMessage {
+	my ( $data, $event )       = @_;
+	my ( $nick, $what ) = @$data;
+
+	my $network = get_info('network');
+	my $channel = get_info('channel');
+
+	if ( grep { lc($_) eq lc($network) } @networkNames ) {
+
+		#If the NICK has not been pushed into the list then
+		#it needs to be done and hopefully before the emit.
+		if ( not exists $nnr{$network}->{$nick} ) {
+			whoGet( $network, $nick );
+		}
+		elsif ( exists $nnr{$network}->{$nick} ) {
+			my $realname = ( split( /\^0|\^1/, $nnr{$network}->{$nick} ) )[0];
+			emit_print( $event, $realname, $what );
+			return EAT_XCHAT;
+		}
+	}
+	return EAT_NONE;
+}
+# |END| Private Message
+
+
+# |BEGIN| Channel Modes(h,o,v)
 #To keep things simple and short(KISS)
 #Add "Text Events" that are ONLY mode changes
 #This would be op, halfop, voice and so on...
@@ -62,24 +92,30 @@ for my $event (
 	hook_print( $event, \&eventChannelMode, { 'data' => $event } );
 }
 
-sub eventMode {
+sub eventChannelMode {
 	my ( $data, $event ) = @_;
-	my ( $by, $to )    = @$data;
+	my ( $by, $to )      = @$data;
 	my $network = get_info('network');
 
-	if ( grep { lc($_) eq lc($network) } @networkNames and
-		 exists $nnr{$network}->{$by}                  and
-		 exists $nnr{$network}->{$to} )
-	{
-		my $realname0 = ( split( /\^0|\^1/, $nnr{$network}->{$by} ) )[0];
-		my $realname1 = ( split( /\^0|\^1/, $nnr{$network}->{$to} ) )[0];
-		emit_print( $event, $realname0, $realname1 );
-		return EAT_XCHAT;
+	if ( grep { lc($_) eq lc($network) } @networkNames ) {
+		#Don't need to who both if only one needs to be who'ed
+		if ( not exists $nnr{$network}->{$by} ){
+			whoGet( $network, $by );
+		}
+		if ( not exists $nnr{$network}->{$to} ){
+			whoGet( $network, $to );
+		}
+		elsif (exists $nnr{$network}->{$by} and exists $nnr{$network}->{$to} ){
+			my $realname0 = ( split( /\^0|\^1/, $nnr{$network}->{$by} ) )[0];
+			my $realname1 = ( split( /\^0|\^1/, $nnr{$network}->{$to} ) )[0];
+			emit_print( $event, $realname0, $realname1 );
+			return EAT_XCHAT;
+		}
 	}
 	return EAT_NONE;
 }
+# |END| Channel Modes(h,o,v)
 
-#|END| Channel Modes(h,o,v)
 
 #Add or change a user when the /RCHG command is executed
 hook_server( 'RCHG', sub {
@@ -104,21 +140,37 @@ hook_server( 'RCHG', sub {
 				$nnr{$network}->{$nick} = $nameNew;
 				emit_print( 'Change Nick', $nameOld, $nameNew );
 			}
-				#US
-				elsif ( $nameNew ne $nameOld and user_info()->{nick} eq $nick ) {
-					$nnr{$network}->{$nick} = $nameNew;
-					emit_print( 'Your Nick Changing', $nameOld, $nameNew );
-				}
+			#Us
+			elsif ( $nameNew ne $nameOld and user_info()->{nick} eq $nick ) {
+				$nnr{$network}->{$nick} = $nameNew;
+				emit_print( 'Your Nick Changing', $nameOld, $nameNew );
+			}
 		}
 	}
 	return EAT_ALL;
 });
+
+
+hook_print( 'Part', sub {
+	my ( $nick, $host, $channel ) = @{ $_[0] };
+
+	my $network = get_info('network');
+
+	if ( exists $nnr{$network}->{$nick} ) {
+		my $realname = ( split( /\^0|\^1/, $nnr{$network}->{$nick} ) )[0];
+		emit_print( 'Part', $realname, $host, $channel );
+		return EAT_XCHAT;
+	}
+	return EAT_NONE;
+});
+
 
 hook_print( 'You Join', sub {
 	my ( $me, $channel, $host ) = @{ $_[0] };
 	whoGet( get_info('network'), $channel );
 	return EAT_NONE;
 });
+
 
 hook_print( 'Your Message', sub {
 	my ( $nick, $what, $mode ) = @{ $_[0] };
@@ -128,16 +180,23 @@ hook_print( 'Your Message', sub {
 	return EAT_NONE unless $network;
 
 	#Only continue if the channel message is from one of the wanted networks
-	if ( grep { lc($_) eq lc($network) } @networkNames and
-		 exists $nnr{$network}->{$nick} )
-	{
-		my $realname = ( split( /\^0|\^1/, $nnr{$network}->{$nick} ) )[0];
-		emit_print( 'Your Message', $realname, $what, $mode );
-		#user_info( $nick )->{realname} <-- Not reliable
-		return EAT_XCHAT;
+	if ( grep { lc($_) eq lc($network) } @networkNames ) {
+
+		#If the NICK has not been pushed into the list then
+		#it needs to be done and hopefully before the emit.
+		if ( not exists $nnr{$network}->{$nick} ) {
+			whoGet( $network, $nick );
+		}
+		elsif ( exists $nnr{$network}->{$nick} ) {
+			my $realname = ( split( /\^0|\^1/, $nnr{$network}->{$nick} ) )[0];
+			emit_print( 'Your Message', $realname, $what, $mode );
+			#user_info( $nick )->{realname} <-- Not reliable
+			return EAT_XCHAT;
+		}
 	}
 	return EAT_NONE;
 });
+
 
 sub whoGet {
 
@@ -145,7 +204,7 @@ sub whoGet {
 	#$channel can be either a channel or nick
 	my ( $network, $channel ) = @_;
 
-	if ( $network and $channel ){     #Make sure...
+	if ( $network and $channel ) {    #Make sure...
 
 		command( 'WHO ' . $channel ); #Wasn't needed before
 
@@ -159,7 +218,7 @@ sub whoGet {
                              ){
                                   $nnr{$network}->{ $_[0][7] } = $_[1][10];
                              }
-                          return EAT_XCHAT;
+                          return EAT_ALL;
 					  });
 
 		#At the end of the /who unhook each numeric event
@@ -167,7 +226,7 @@ sub whoGet {
 		$who_end = hook_server( '315', sub {
 					   unhook($hooked_who);
 				       unhook($who_end);
-				       return EAT_XCHAT;
+				       return EAT_ALL;
 			       });
 	}
 }
