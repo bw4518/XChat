514b9097d7908c5dd8a779c00c3942d5ca4f495b
culb <jessejlutz@gmail.com>
2015-04-06 02:55:29 -0400

    Complete JOIN events
    Two different ways to handle the JOIN event. One is directly in the Join
    event. The other eats the JOIN event and lets the Channel Message handle
    it when ^Status is done. Other minor edits as well.

--- 4x4EvolutionNormalizer.pl
+++ 4x4EvolutionNormalizer.pl
@@ -2,10 +2,19 @@
 #Purpose: Show realnames instead of nicks to mimic 4x4 Evolution
 #Version: 1 - Complete rewrite
 
+# Shit that needs accomplished
+# 1: If XChat is already and the script gets load,
+#    it needs to search the connected networks and
+#    if any are one of the networks then handle it ( see code at bottom )
+# 2: Handle misc events that I forgot or overlooked
+# 3: Fix any mistakes I have made
+# 4: USRIP to generate NICK-PORT
+# 5: This is a big one... Connect in the sequence the game connects.
+#    Usually this requires a source code edit
+
 use strict;
 use warnings;
 use Xchat qw(:all);
-use Data::Dumper;
 
 #Add the network(s) this script should work with
 my @networkNames = qw( fuzzy personal );
@@ -22,13 +31,18 @@ sub eventChannelMessage {
 	my ( $data, $event )       = @_;
 	my ( $nick, $what, $mode ) = @$data;
 
+	#undef $mode if not $mode;
+	
 	my $network = get_info('network');
 	my $channel = get_info('channel');
 
 	if ( grep { lc($_) eq lc($network) } @networkNames ) {
-
+		
 		#That dirty little message the client sends when it joins a channel
+		#emit a join message here since there is a realname to send in it
 		if ( $what =~ m/^\^STATUS\s(.+)/g ) {
+			my $realname = ( split( /\^0|\^1/, $1 ) )[0];
+			emit_print( 'Join', $realname, $channel, user_info($nick)->{host} );
 			return EAT_XCHAT;
 		}
 
@@ -76,6 +90,33 @@ sub eventPrivateMessage {
 	return EAT_NONE;
 }
 # |END| Private Message
+
+
+# |BEGIN| Notice
+# Opers can use this to communicate without others seeing
+# So you can do channel notices and noone will see in game.
+for my $event ( 'Notice', 'Notice Send' ) {
+	hook_print( $event, \&eventNotice, { 'data' => $event } );
+}
+
+sub eventNotice {
+	my ( $data, $event ) = @_;
+	my ( $nick, $what )  = @$data;
+	my $network = get_info('network');
+
+	if ( grep { lc($_) eq lc($network) } @networkNames ) {
+
+		if ( not exists $nnr{$network}->{$nick} and $nick !~ /^#/) {
+			whoGet( $network, $nick );
+		}
+		elsif ( exists $nnr{$network}->{$nick} ) {
+			my $realname = ( split( /\^0|\^1/, $nnr{$network}->{$nick} ) )[0];
+			emit_print( $event, $realname, $what );
+			return EAT_XCHAT;
+		}
+	}
+}
+# |END| Notice
 
 
 # |BEGIN| Channel Modes(h,o,v)
@@ -157,12 +198,92 @@ hook_print( 'Close_Context', sub {
 
 	#Proceed if it is a server context and from out list of networks
 	if ( context_info->{type} == '1' and
-         grep { lc($_) eq lc($network) } @networkNames) {
-
-		delete $nnr{ $network } if exists $nnr{ $network };
+		 exists $nnr{ $network }     and
+         grep { lc($_) eq lc($network) } @networkNames
+       ){
+			delete $nnr{ $network };
 	}
 	return EAT_NONE;
 });
+
+
+
+##########################################################################
+##########################################################################
+# FIX ME
+# A Client sends an RCHG and ^STATUS message when it joins with its realname
+# Need to wait until those are done ( since they populate the hash )
+# and THEN emit the new JOIN hook
+#
+# Until then this is pointless...
+#
+# UPDATE
+# So I did some thinking and came up with what you are about to see
+# Here is how it works
+#
+# The real client sends a PRIVMSG beginning with ^STATUS
+# when it joins to let the other clients know it joined the channel.
+# Since we know it will send this we can use it as a point to emit the join event
+#
+# 1: The JOIN hook_print hooks the join events for all of XChat
+# 2: Check if this happened on a server specified in @networknames (Duh)
+# 3: Hook the message and then and use the real name in it to add to the emit
+# 4: $1 contains the realname in the ^STATUS message
+# 5: Unhook the PRIVMSG event and the JOIN event
+# 
+#
+# While typing these comments I came up with a better solution
+# $hookJoin being global doesn't sit well with me.
+#
+#my $hookJoin; # Global :-(
+#$hookJoin =
+#hook_print( 'Join', sub {
+#	my ( $nick, $channel, $host ) = @{ $_[0] };
+#
+#	my $network = get_info('network');
+#
+#	if ( grep { lc($_) eq lc($network) } @networkNames ) {
+#		
+#		my $hookPRIVMSG;
+#		$hookPRIVMSG = hook_server('PRIVMSG', sub{
+#			# Host    $_[0][0];
+#			# Event   $_[0][1];
+#			# Channel $_[0][2];
+#			# Message $_[1][3];
+#			if (
+#					lc($_[0][2]) eq lc($channel)    and
+#					$_[1][3] =~ m/^:\^STATUS\s(.+)/ and
+#					exists $nnr{$network}->{$nick}
+#				)
+#				{
+#					my $realname = ( split( /\^0|\^1/, $1 ) )[0];
+#					emit_print( 'Join', $realname, $channel, $host );
+#				}
+#			unhook($hookPRIVMSG);
+#		});
+#		unhook($hookJoin);
+#		return EAT_XCHAT;
+#	}
+#	return EAT_NONE;
+#});
+##########################################################################
+##########################################################################
+
+
+#EAT the event since we handle it in the channel messages
+hook_print( 'Join', sub {	
+	my ( $nick, $channel, $host ) = @{ $_[0] };
+
+	my $network = get_info('network');
+
+	if ( grep { lc($_) eq lc($network) } @networkNames and
+         $nick =~ /[A-P]{12}|^_/ #A user can potentially hide this event
+       ) {
+			return EAT_XCHAT;
+	}
+	return EAT_NONE;
+});
+
 
 hook_print( 'Part', sub {
 	my ( $nick, $host, $channel ) = @{ $_[0] };
@@ -172,10 +293,12 @@ hook_print( 'Part', sub {
 	if ( exists $nnr{$network}->{$nick} ) {
 		my $realname = ( split( /\^0|\^1/, $nnr{$network}->{$nick} ) )[0];
 		emit_print( 'Part', $realname, $host, $channel );
+		delete $nnr{$network}->{$nick};
 		return EAT_XCHAT;
 	}
 	return EAT_NONE;
 });
+
 
 hook_print( 'Quit', sub {
 	my ( $nick, $reason, $host ) = @{ $_[0] };
@@ -185,10 +308,12 @@ hook_print( 'Quit', sub {
 	if ( exists $nnr{$network}->{$nick} ) {
 		my $realname = ( split( /\^0|\^1/, $nnr{$network}->{$nick} ) )[0];
 		emit_print( 'Quit', $realname, $reason, $host );
+		delete $nnr{$network}->{$nick};
 		return EAT_XCHAT;
 	}
 	return EAT_NONE;
 });
+
 
 hook_print( 'You Join', sub {
 	my ( $me, $channel, $host ) = @{ $_[0] };
@@ -206,23 +331,25 @@ hook_print( 'You Join', sub {
 	#We need to detect the end of /names and then what we need to do.
 	#Let's do this....
 
-	my $namesEnd;
-	$namesEnd = hook_server('366', sub {
-					if ( lc($_[0][3]) eq lc($channel) ){
-						
-						# We will who the channel in a little bit and add our realname
-						# to the hash then so for now use get_prefs() for our realname
-						command( 'QUOTE RCHG :' . get_prefs( 'irc_real_name' ) );
-						command( 'QUOTE PRIVMSG ' . $channel . ':^STATUS ' . get_prefs( 'irc_real_name' ) );
+	if ( grep { lc($_) eq lc($network) } @networkNames ) {
+		my $namesEnd;
+		$namesEnd = hook_server('366', sub {
+						if ( lc($_[0][3]) eq lc($channel) ){
 
-						# Just an example if we were follow the order but XChat does this already
-						# and we don't give a shit when we get the topic.
-						# command( 'TOPIC ' . $channel ); 
+							# We will who the channel in a little bit and add our realname
+							# to the hash then so for now use get_prefs() for our realname
+							command( 'QUOTE RCHG :' . get_prefs( 'irc_real_name' ) );
+							command( 'QUOTE PRIVMSG ' . $channel . ' :^STATUS ' . get_prefs( 'irc_real_name' ) );
 
-						whoGet( $network, $channel );
-                    }
-                    unhook($namesEnd);
-                });
+							# Just an example if we were follow the order but XChat does this already
+							# and we don't give a shit when we get the topic.
+							# command( 'TOPIC ' . $channel ); 
+
+							whoGet( $network, $channel );
+	                    }
+	                    unhook($namesEnd);
+	                });
+	}
 	return EAT_NONE;
 });
 
@@ -286,7 +413,15 @@ sub whoGet {
 	}
 }
 
-=pod
+
+register(
+	'Realnames',
+	0x3DFB3F,
+	'Replace NICKS in the Text Events with realnames to mimic 4x4 Evolution'
+);
+
+
+__END__
 
 #FIX ME!!
 #Might not be broke but I forget what needed fixed.
@@ -349,7 +484,3 @@ sub init_1{
 	}
 	set_context $context;
 }
-=cut
-
-register( 'Realnames', 1,
-	'Replace NICKS in the Text Events with realnames to mimic 4x4 Evolution' );
