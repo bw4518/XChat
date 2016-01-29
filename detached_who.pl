43dd2be96edde665ecb3b0fd74f010916f277487
culb <jessejlutz@gmail.com>
2016-01-28 20:16:20 -0500

    Sanitation
    Clean up the code for easier reading. Add some noted. Other general
    clean up.

--- detached_who.pl
+++ detached_who.pl
@@ -1,5 +1,9 @@
-# Author: culb ( A.K.A nightfrog )
-#Create a command 'DEWHO' to show the output of /WHO in a separate window
+# Author: culb ( A.K.A. nightfrog )
+# Show the output of /WHO in a separate window
+#
+# Usage: Type /DEWHO in a channel or PM context
+#
+# TODO: Let the user supply a nick or channel
 
 use strict;
 use warnings;
@@ -7,10 +11,8 @@ use Gtk2 -init;
 use Gtk2::SimpleList;
 use Xchat qw( :all );
 
-
-#Keep a record of windows to destroy
+#Keep track of windows to destroy when the script is unloaded
 my @record;
-
 
 register(
     'Detached WHO',
@@ -23,87 +25,81 @@ register(
 use constant TRUE  => 1;
 use constant FALSE => 0;
 
+hook_command 'DEWHO' => sub
+{
+    my $contextType = context_info->{type};
 
-hook_command( 'DEWHO',
-    sub
+    if( $contextType == 2 or $contextType == 3 )
     {
+        my $window = Gtk2::Window->new;
+        $window->set_title( 'Detached WHO' );
+        $window->set_default_size( 500, 300 );
 
-        #Channels or dialogs
-        if( context_info->{type} == 2 or context_info->{type} == 3 )
+        my $hbox = Gtk2::HBox->new;
+        $window->add( $hbox );
+
+        my $sList = Gtk2::SimpleList->new( 'Network'   => 'text',
+                                           'Channel'   => 'text',
+                                           'User name' => 'text',
+                                           'Host'      => 'text',
+                                           'Server'    => 'text',
+                                           'Nick'      => 'text',
+                                           'Modes'     => 'text',
+                                           'HOPS'      => 'text',
+                                           'Real name' => 'text' );
+
+        if ( my $network = get_info 'network' and my $channel = get_info 'channel' )
         {
-            my $window = Gtk2::Window->new;
-            $window->set_title( 'Detached WHO' );
-            $window->set_default_size( 500, 250 );
+            command( 'QUOTE WHO ' . $channel );
 
-            my $hbox = Gtk2::HBox->new;
-            $window->add( $hbox );
+            my $hookedWho;
+            $hookedWho = hook_server '352' => sub
+                          {
+                              my $hops = $_[0][9];
+                              $hops =~ s/^://;
+                              push @{ $sList->{data} }, [ $network, $_[0][3], $_[0][4],
+                                                          $_[0][5], $_[0][6], $_[0][7],
+                                                          $_[0][8], $hops   , $_[1][10] ];
+                              return EAT_XCHAT;
+                          };
 
-            my $slist = Gtk2::SimpleList->new ( 'Network'   => 'text',
-                                                'Channel'   => 'text',
-                                                'User name' => 'text',
-                                                'Host'      => 'text',
-                                                'Server'    => 'text',
-                                                'Nick'      => 'text',
-                                                'Modes'     => 'text',
-                                                'HOPS'      => 'text',
-                                                'Real name' => 'text' );
-
-            if ( my $network = get_info 'network' and my $channel = get_info 'channel' )
-            {
-                #$channel is a channel or nick depending the on context
-                command( 'QUOTE WHO ' . $channel );
-
-                my $hook_who;
-                $hook_who = hook_server( '352',
-                sub
-                {
-                    #Remove the leading : from the hops
-                    my $string = $_[0][9];
-                    $string =~ s/^://;
-
-                    push @{ $slist->{data} }, [ $network, $_[0][3], $_[0][4], $_[0][5],
-                                                $_[0][6], $_[0][7], $_[0][8], $string , $_[1][10] ];
-                });
-
-                my $hook_who_end;
-                $hook_who_end = hook_server( '315',
-                sub
-                {
-                    unhook $hook_who;
-                    unhook $hook_who_end;
-                });
-            }
-
-
-
-
-
-            #Editable fields for copying information
-            $slist->set_column_editable( $_, TRUE ) for 0..8;
-
-            #Reorder rows
-            $slist->set_reorderable( TRUE );
-
-            #Resize columns
-            map { $_->set_resizable( TRUE ) } $slist->get_columns;
-
-            #Scrollable window
-            my $scrolled = Gtk2::ScrolledWindow->new;
-            $scrolled->set_policy( 'automatic', 'automatic' );
-            $scrolled->add( $slist );
-            $hbox->add( $scrolled );
-
-            #Show the window
-            $window->show_all;
-
-            #Keep a record of windows to destroy
-            push @record, $window if $window;
+            my $hookedWhoEnd;
+            $hookedWhoEnd = hook_server '315' => sub
+                            {
+                                unhook $hookedWho;
+                                unhook $hookedWhoEnd;
+                                return EAT_XCHAT;
+                            };
         }
 
-        return EAT_XCHAT;
 
-    },
-    {
-        help_text => 'Create a detached window with the output of /WHO'
+
+
+
+        #Editable fields for copying information
+        $sList->set_column_editable( $_, TRUE ) for 0..8;
+
+        #Reorder rows
+        $sList->set_reorderable( TRUE );
+
+        #Resize columns
+        map { $_->set_resizable( TRUE ) } $sList->get_columns;
+
+        #Scrollable window
+        my $scrolled = Gtk2::ScrolledWindow->new;
+        $scrolled->set_policy( 'automatic', 'automatic' );
+        $scrolled->add( $sList );
+        $hbox->add( $scrolled );
+
+        #Show the window
+        $window->show_all;
+
+        #Keep track of windows to destroy
+        push @record, $window if $window;
     }
-);
+
+    return EAT_XCHAT;
+},
+{
+    help_text => '/DEWHO - Create a detached window with the output of /WHO'
+};
